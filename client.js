d92a81958274177de7b77a44ae6689232823ed37
Hoai Viet Nguyen <hvtnguyen@yahoo.de>
2012-04-18 15:40:46 +0200

    timestamp and node.log

--- client.js
+++ client.js
@@ -1,5 +1,7 @@
 var client = require("socket.io-client");
-var host = "http://localhost:3000";
+var host = "http://176.34.100.20:3000";
+var fs = require('fs');
+var nodeLog = fs.createWriteStream(__dirname+'/node.log',{flags:'a',mode:0666, encoding:'encoding'});
 
 //host = "http://localhost:3000";
 var max =10000;
@@ -8,13 +10,18 @@ var min = 1000;
 var x=(Math.random() * (max - min)) + min;
 var socket = client.connect(host);
 
+var i = 0;
 
 setInterval(function(){
+	
 	x = (Math.random() * (max - min)) + min;
 	socket.emit("testTime",new Date().getTime());
-},x);
+	
+},1000);
 
-process.on("exit",function(){
-	socket.emit("testTime","Im disconnected");
-	socket.disconnect();
+socket.on("testTime",function(command){
+	var log = 
+	nodeLog.write(new Date().getTime() - command.time +"\n");
+	//console.log(command.id);
+	
 });
